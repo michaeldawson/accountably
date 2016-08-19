5f3a03ee2d610db984a1ad79a8ee0419302e7fd9
Michael Dawson <mdawson@marketplacer.com>
2016-08-19 17:58:05 +1000

    Add accounts

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:account) { Account.new(valid_attributes) }
+  let(:valid_attributes) {
+    {
+      user: User.new
+    }
+  }
+
+  describe 'Validation' do
+    it 'should be valid with valid attributes' do
+      expect(account).to be_valid
+    end
+
+    it "shouldn't be valid without a user" do
+      valid_attributes[:user] = nil
+      expect(account).not_to be_valid
+    end
+  end
 end
