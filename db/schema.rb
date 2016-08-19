567d6300b1cee939f58443153e90b7e01515158d
Michael Dawson <mdawson@marketplacer.com>
2016-08-19 17:54:28 +1000

    Add accounts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819075219) do
+ActiveRecord::Schema.define(version: 20160819075416) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.integer  "balance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "accounts", ["user_id"], name: "index_accounts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
