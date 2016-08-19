e084701517ae374f53c66a81eb0568af8c028f6c
Michael Dawson <mdawson@marketplacer.com>
2016-08-19 18:54:32 +1000

    Setup database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,4 +15,20 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, feature: true) do
+    DatabaseCleaner.strategy = :deletion
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning { example.run }
+  end
 end
