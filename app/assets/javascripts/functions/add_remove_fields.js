171f12d1dbe5c5ac49f896d933597c2676cecf2d
Michael Dawson <mdawson@marketplacer.com>
2016-08-20 13:57:12 +1000

    Add jquery ui widget, add/remove fields js

---
+++ app/assets/javascripts/functions/add_remove_fields.js
@@ -0,0 +1,28 @@
+$(document).on('ready page:load page:restore', function() {
+  $('.add_remove_fields').addRemoveFields();
+});
+
+$.widget('custom.addRemoveFields', {
+  _create: function() {
+    this.$element = $(this.element);
+
+    this.$element.on('click', '.add_fields', this._handleAddFields);
+    this.$element.on('click', '.remove_fields', this._handleRemoveFields);
+  },
+
+  _handleAddFields: function(e) {
+    var regexp = new RegExp($(this).data('id'), 'g'),
+        time   = new Date().getTime();
+
+    var content   = $(this).data('fields').replace(regexp, time);
+    $(this).before(content);
+
+    e.preventDefault();
+  },
+
+  _handleRemoveFields: function(e) {
+    $(this).prev('input[type=hidden]').val('1');
+    $(this).closest('fieldset').hide();
+    e.preventDefault();
+  },
+});
