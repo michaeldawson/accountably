464a37e1ce02a3b4c79293f8d7667b19ddd40e87
Michael Dawson <mdawson@marketplacer.com>
2016-09-23 17:51:54 +1000

    Persist rspec examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.example_status_persistence_file_path = 'spec/examples.txt'
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
