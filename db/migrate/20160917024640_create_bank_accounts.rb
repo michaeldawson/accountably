aed968672c600135d1406e2bb04198841f70f4db
Michael Dawson <mdawson@marketplacer.com>
2016-10-01 15:48:57 +1000

    Fix database migration ordering issue

--- db/migrate/20160917024640_create_bank_accounts.rb
+++ db/migrate/20160917024640_create_bank_accounts.rb
@@ -1,7 +1,7 @@
 class CreateBankAccounts < ActiveRecord::Migration
   def change
     create_table :bank_accounts do |t|
-      t.references :login, index: true, foreign_key: true
+      t.references :login, index: true, foreign_key: { to_table: :bank_logins }
       t.string :name
       t.datetime :sync_from
 
